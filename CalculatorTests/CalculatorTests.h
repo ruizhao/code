107bb1fa237abb466d41f08b0d64b1e1550afaee
Rui Zhao <rzhao@zynga.com>
2013-03-11 00:32:46 -0400

    add2

---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Rui Zhao on 3/9/13.
+//  Copyright (c) 2013 Rui Zhao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
